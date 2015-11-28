36ded82194b45e6f408a2f455137aeb4c8bb53e4
Raider43 <rex41043@gmail.com>
2015-11-29 00:21:16 +1300

    Update

--- termnial/load.js
+++ termnial/load.js
@@ -1,11 +1,14 @@
-//var open = new Audio("https://dl.dropboxusercontent.com/u/55126910/fallout/sounds/ui_hacking_passgood.wav");
-//open.volume = .5;
-//open.play();
+window.addEventListener('load', 
+	function() { 
+		var open = new Audio("http://defendervex.github.io/termnial/boot_sound.wav");
+		open.volume = .5;
+		open.play();
 
-var loop = new Audio("fan_loop.wav"); 
-loop.addEventListener('ended', function() {
-	this.currentTime = 0;
-	this.play();
-}, false);
-loop.volume = .1;
-loop.play();
+		var loop = new Audio("http://defendervex.github.io/termnial/fan_loop.wav"); 
+		loop.addEventListener('ended', function() {
+			this.currentTime = 0;
+			this.play();
+		}, false);
+		loop.volume = .1;
+		loop.play();
+ }, false);
