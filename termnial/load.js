f2550339496c669cf56589ce03c2225edf0bf1e8
Raider43 <rex41043@gmail.com>
2015-11-29 00:23:52 +1300

    Update

--- termnial/load.js
+++ termnial/load.js
@@ -1,14 +1,11 @@
-window.addEventListener('load', 
-	function() { 
-		var open = new Audio("http://defendervex.github.io/termnial/boot_sound.wav");
-		open.volume = .5;
-		open.play();
+var open = new Audio("http://defendervex.github.io/termnial/boot_sound.wav");
+open.volume = .5;
+open.play();
 
-		var loop = new Audio("http://defendervex.github.io/termnial/fan_loop.wav"); 
-		loop.addEventListener('ended', function() {
-			this.currentTime = 0;
-			this.play();
-		}, false);
-		loop.volume = .1;
-		loop.play();
- }, false);
+var loop = new Audio("http://defendervex.github.io/termnial/fan_loop.wav"); 
+loop.addEventListener('ended', function() {
+	this.currentTime = 0;
+	this.play();
+}, false);
+loop.volume = .1;
+loop.play();
