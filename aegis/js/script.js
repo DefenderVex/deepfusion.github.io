278144bf1cacc9516c2d0b296391deb4e7dd44f8
DefenderVex <rex41043@gmail.com>
2016-11-27 20:01:47 +1300

    Update

--- aegis/js/script.js
+++ aegis/js/script.js
@@ -1,17 +1,17 @@
 (function($) {
     "use strict";
-	
-	$('.page-scroll a').bind('click', function(event) {
-		var $anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: ($($anchor.attr('href')).offset().top - 50)
-		}, 1250, 'easeInOutExpo');
-		event.preventDefault();
-	});
-	
-	$('body').scrollspy({
+
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     });
-	
+
 })(jQuery);
