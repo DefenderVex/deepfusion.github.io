043839152f8b7cfe517c4728fc0e59f0134b0abb
DefenderVex <DefenderVex@users.noreply.github.com>
2018-05-08 23:12:48 +1200

    Clear Repo

--- archive/generalionics/python/generate_shareholders.py
+++
@@ -1,9 +0,0 @@
-import evelink.api
-
-corpID = 98414469 # ID of the corporation.
-
-api = evelink.api.API()
-corp = evelink.corp.Corp(api)
-response = corp.corporation_sheet(corpID)
-
-print(evelink.api.get_named_value("ceo", "id"))
